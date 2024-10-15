1c6fdd0732ec5c2edfd6fd5ccffa431586e73993
FARAZIFTAN <mahasiswaulbiit@gmail.com>
2024-10-15 16:25:00 +0700

    pertemuan02

---
+++ pertemuan02/praktikum/P2_1_714230064/P2_1_714230064/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714230064
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukan angka pertama : ");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Masukan angka kedua : ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{input1} + {input2} = {input1 + input2}");
+            Console.WriteLine($"{input1} - {input2} = {input1 - input2}");
+            Console.WriteLine($"{input1} * {input2} = {input1 * input2}");
+            Console.WriteLine($"{input1} / {input2} = {input1 / input2}");
+
+        }
+    }
+}
